66fd2642b624226fbc6b556fb9ba98a8eb63b36d
Marcos Fraga <marcosjsfraga@gmail.com>
2020-06-06 15:37:16 -0300

    Changes

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -4,7 +4,8 @@ import { Link, useHistory } from 'react-router-dom';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import api from '../../services/api';
 import axios from 'axios';
-import { LeafletMouseEvent } from 'leaflet';
+import { LeafletMouseEvent } from 'leaflet'; 
+import Dropzone from '../../components/Dropzone';
 
 import './styles.css';
 
@@ -40,7 +41,8 @@ const CreatePoint = () => {
     const [selectedUf, setSelectedUf] = useState('0');
     const [selectedCity, setSelectedCity] = useState('0');
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
-    const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+	const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+	const [selectedFile, setSelectedFile] = useState<File>();
 
     const history = useHistory();
     
@@ -112,18 +114,19 @@ const CreatePoint = () => {
         const uf = selectedUf;
         const city = selectedCity;
         const [latitude, longitude] = selectedPosition;
-        const items = selectedItems;
-
-        const data = {
-            name, 
-            email, 
-            whatsapp,
-            uf,
-            city,
-            latitude, 
-            longitude,
-            items
-        };
+		const items = selectedItems;
+		
+		const data = new FormData();
+		data.append('name', name);
+		data.append('email', email);
+		data.append('whatsapp', whatsapp);
+		data.append('uf', uf);
+		data.append('city', city);
+		data.append('latitude', String(latitude));
+		data.append('longitude', String(longitude));
+		data.append('items', items.join(','));
+		if (selectedFile)
+			data.append('image', selectedFile );
 
         await api.post('points', data);
         
@@ -144,6 +147,9 @@ const CreatePoint = () => {
 
             <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br/> ponto de coleta</h1>
+
+				<Dropzone onFileUploaded={setSelectedFile} />
+				
                 {/* Dados */}
                 <fieldset>
                     <legend>
